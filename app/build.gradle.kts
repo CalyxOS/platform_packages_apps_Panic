95d09d1406dd54f72b3e4e8a5535cc48c5c86d09
Aayush Gupta <aayushgupta219@gmail.com>
2023-09-24 22:40:01 +0530

    Migrate Gradle build scripts to Kotlin DSL
    * Kotlin DSL is the new default DSL for Android Projects
    
    Ref: https://developer.android.com/build/migrate-to-kotlin-dsl
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I73f4fac2fe7c8605cceaa0cb6e430616d48b91e3

---
+++ app/build.gradle.kts
@@ -0,0 +1,91 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+plugins {
+    id("com.android.application")
+    id("com.google.dagger.hilt.android")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
+    id("org.jlleitschuh.gradle.ktlint")
+}
+
+android {
+    namespace = "org.calyxos.panic"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "org.calyxos.panic"
+        minSdk = 33
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    signingConfigs {
+        create("aosp") {
+            // Generated from the AOSP testkey:
+            // https://android.googlesource.com/platform/build/+/refs/tags/android-11.0.0_r29/target/product/security/testkey.pk8
+            keyAlias = "testkey"
+            keyPassword = "testkey"
+            storeFile = file("testkey.jks")
+            storePassword = "testkey"
+        }
+    }
+    buildTypes {
+        debug {
+            signingConfig = signingConfigs.getByName("aosp")
+        }
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+    lint {
+        lintConfig = file("lint.xml")
+    }
+}
+
+kapt {
+    correctErrorTypes = true
+}
+
+hilt {
+    enableAggregatingTask = false
+}
+
+dependencies {
+
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.preference:preference-ktx:1.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+
+    val hiltVersion = "2.45"
+    kapt("com.google.dagger:hilt-compiler:$hiltVersion")
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+
+}
