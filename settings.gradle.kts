8cacb7763d3f286cadf3e1542b32571bf5e31ba4
Aayush Gupta <aayushgupta219@gmail.com>
2023-09-24 22:40:01 +0530

    Migrate Gradle build scripts to Kotlin DSL
    * Kotlin DSL is the new default DSL for Android Projects
    
    Ref: https://developer.android.com/build/migrate-to-kotlin-dsl
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I73f4fac2fe7c8605cceaa0cb6e430616d48b91e3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 pluginManagement {
     repositories {
         google()
@@ -13,4 +18,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "Panic"
-include ':app'
+include(":app")
