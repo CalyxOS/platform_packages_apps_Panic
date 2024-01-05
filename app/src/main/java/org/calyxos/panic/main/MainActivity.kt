cd017442080a12eac215e6c7e1ea72581330d3d6
Aayush Gupta <aayushgupta219@gmail.com>
2024-01-05 22:28:56 +0530

    MainActivity: Move logic to MainFragment
    Let everything related to UI get setup to avoid race conditions with
    navController
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I67dc42a7f8fc9d5b357b7d0f6704e71e7c96708e

--- app/src/main/java/org/calyxos/panic/main/MainActivity.kt
+++ app/src/main/java/org/calyxos/panic/main/MainActivity.kt
@@ -6,46 +6,15 @@
 package org.calyxos.panic.main
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import androidx.preference.PreferenceManager
 import dagger.hilt.android.AndroidEntryPoint
-import info.guardianproject.panic.Panic
-import info.guardianproject.panic.PanicResponder
 import org.calyxos.panic.R
 
 @AndroidEntryPoint(AppCompatActivity::class)
 class MainActivity : Hilt_MainActivity() {
 
-    private val viewModel: MainActivityViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        when (intent?.action) {
-            Panic.ACTION_TRIGGER -> {
-                val prefManager = PreferenceManager.getDefaultSharedPreferences(this)
-                viewModel.uninstallPanicApps()
-                if (prefManager.getBoolean("exit_app", true)) finish()
-            }
-            Panic.ACTION_CONNECT -> {
-                val navHostFragment =
-                    supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-
-                // Show confirmation dialog if triggering package is a new one
-                val intentReferer = PanicResponder.getConnectIntentSender(this)
-                val triggerPkgName = PanicResponder.getTriggerPackageName(this)
-
-                if (intentReferer.isNotBlank() && intentReferer != triggerPkgName) {
-                    navHostFragment.navController.navigate(R.id.confirmationDialogFragment)
-                }
-            }
-            Panic.ACTION_DISCONNECT -> {
-                PanicResponder.checkForDisconnectIntent(this)
-                finish()
-            }
-        }
     }
 }
