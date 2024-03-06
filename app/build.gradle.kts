babfda56bbe258b0cd1311c41c58c0a3a23555d7
Aayush Gupta <aayushgupta219@gmail.com>
2024-03-06 12:05:32 +0530

    gradle: Bump and pin dependencies version as in AOSP QPR2
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Ib0a7cbdc951fc22a9fc658bab2b7f900d406fdb6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,24 +68,53 @@ hilt {
 }
 
 dependencies {
-
+    /**
+     * Dependencies in AOSP
+     *
+     * We try to keep the dependencies in sync with what AOSP ships as Panic is meant to be
+     * built with the AOSP build system and gradle builds are just for more pleasant development.
+     * Using the AOSP versions in gradle builds allows us to spot issues early on.
+     */
     compileOnly(fileTree(mapOf("dir" to "libs", "include" to listOf("android.jar", "libcore.jar"))))
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("info.guardianproject.panic-1.0.jar"))))
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    // AndroidX
+    // https://android.googlesource.com/platform/prebuilts/sdk/+/android-14.0.0_r29/current/androidx/m2repository/androidx/core/core-ktx/1.13.0-alpha01/Android.bp
+    implementation("androidx.core:core-ktx") {
+        version { strictly("1.13.0-alpha01") }
+    }
+    // https://android.googlesource.com/platform/prebuilts/sdk/+/android-14.0.0_r29/current/androidx/m2repository/androidx/appcompat/appcompat/1.7.0-alpha04/Android.bp
+    implementation("androidx.appcompat:appcompat") {
+        version { strictly("1.7.0-alpha03") } // 1.7.0-alpha04 in AOSP but isn't released
+    }
+    // https://android.googlesource.com/platform/prebuilts/sdk/+/android-14.0.0_r29/current/androidx/m2repository/androidx/preference/preference/1.3.0-alpha01/Android.bp
+    implementation("androidx.preference:preference-ktx") {
+        version { strictly("1.2.1") } // 1.3.0-alpha01 in AOSP but isn't released
+    }
+    // https://android.googlesource.com/platform/prebuilts/sdk/+/android-14.0.0_r29/current/androidx/m2repository/androidx/lifecycle/lifecycle-viewmodel-ktx/2.7.0-alpha02/Android.bp
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx") {
+        version { strictly("2.7.0-alpha02") }
+    }
+    // https://android.googlesource.com/platform/prebuilts/sdk/+/android-14.0.0_r29/current/androidx/m2repository/androidx/navigation/navigation-fragment-ktx/2.8.0-alpha01/Android.bp
+    implementation("androidx.navigation:navigation-fragment-ktx") {
+        version { strictly("2.8.0-alpha01") }
+    }
+    // https://android.googlesource.com/platform/prebuilts/sdk/+/android-14.0.0_r29/current/androidx/m2repository/androidx/navigation/navigation-ui-ktx/2.8.0-alpha01/Android.bp
+    implementation("androidx.navigation:navigation-ui-ktx") {
+        version { strictly("2.8.0-alpha01") }
+    }
+
+    // Google
+    // https://android.googlesource.com/platform/prebuilts/sdk/+/refs/tags/android-14.0.0_r29/current/extras/material-design-x/Android.bp#15
+    implementation("com.google.android.material:material") {
+        version { strictly("1.7.0-alpha03") }
+    }
+    // https://android.googlesource.com/platform/external/dagger2/+/refs/tags/android-14.0.0_r29
+    val hiltVersion = "2.47"
+    kapt("com.google.dagger:hilt-compiler:$hiltVersion")
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    implementation("androidx.preference:preference-ktx:1.2.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-
-    val hiltVersion = "2.45"
-    kapt("com.google.dagger:hilt-compiler:$hiltVersion")
-    implementation("com.google.dagger:hilt-android:$hiltVersion")
 }
