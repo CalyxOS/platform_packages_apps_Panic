0d74a96852ef5ee84944305523a9acf92e7659cb
Aayush Gupta <aayushgupta219@gmail.com>
2024-03-06 12:05:32 +0530

    gradle: Bump and pin dependencies version as in AOSP
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Ib0a7cbdc951fc22a9fc658bab2b7f900d406fdb6

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.1" apply false
-    id("com.google.dagger.hilt.android") version "2.45" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.8.20" apply false
+    // https://android.googlesource.com/platform/external/kotlinc/+/refs/tags/android-14.0.0_r29/build.txt
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.0" apply false
+    id("com.google.dagger.hilt.android") version "2.47" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1" apply false
 }
